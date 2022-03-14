16166d3815d2fd355d62318e972aa675eb5b225d
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-13 20:45:27 -0600

    commit tree 08fa977c6b88fd3a7de35b29fb648aa69f4359db

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide =function(){ // defined as a function expression with a name "divide"
+    return 2000/100 
+}
+
+const square = num =>{ //arrow function with a name "square"
+    return num * num
+}
+const add = (num1,num2)=>{ // arrow function with a name "add"
+    return num1 + num2
+}
